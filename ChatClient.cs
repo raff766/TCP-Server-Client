b506e4c4d10ed598af18892a124ea3a956ddfe9a
raff766 <61339402+raff766@users.noreply.github.com>
2021-06-24 19:42:55 -0700

    Add files via upload

---
+++ ChatClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class ChatClient {
+    public const string ServerAddress = "localhost";
+    public const int Port = 2020;
+    public const string Name = "Test";
+
+    private TcpClient server;
+
+    static void Main(string[] args)
+    {
+        ChatClient chatClient = new ChatClient();
+        chatClient.Connect();
+
+        while(true) {
+            string input = Console.ReadLine();
+            chatClient.SendMessage(input);
+        }
+    }
+
+    public void Connect() {
+        server = new TcpClient(ServerAddress, Port);
+        Console.WriteLine("Connected to server at {0}.", server.Client.RemoteEndPoint);
+
+        Task.Run(() => BeginRecievingMessages());
+    }
+
+    private void BeginRecievingMessages() {
+        while(true) {
+            byte[] inputBuffer = new byte[server.ReceiveBufferSize];
+            int recievedBytes = server.GetStream().Read(inputBuffer, 0, inputBuffer.Length);
+            if (recievedBytes > 0) {
+                Console.WriteLine(Encoding.UTF8.GetString(inputBuffer, 0, recievedBytes));
+            }
+        }
+    }
+
+    public void SendMessage(string message) {
+        byte[] outputBuffer = Encoding.UTF8.GetBytes(message);
+        server.GetStream().Write(outputBuffer, 0, outputBuffer.Length);
+    }
+}
---
+++ ChatServer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+#pragma warning disable 4014
+
+class ChatServer {
+    private const int Port = 2020;
+    private readonly List<TcpClient> tcpClients = new List<TcpClient>();
+    private TcpListener tcpListener;
+
+    public ChatServer() {
+        tcpListener = new TcpListener(IPAddress.Any, Port);
+    }
+
+    static void Main(string[] args) {
+        new ChatServer().Run();
+    }
+
+    public void Run() {
+        tcpListener.Start();
+        while (true) {
+            TcpClient newClient = tcpListener.AcceptTcpClient();
+            tcpClients.Add(newClient);
+            Console.WriteLine("Created connection with {0}", newClient.Client.RemoteEndPoint);
+            Task.Run(() => BeginRecievingMessages(newClient));
+            SendMessage("Welcome!", newClient);
+        }
+    }
+
+    public void SendMessage(string message, params TcpClient[] tcpClients) {
+        byte[] encodedString = Encoding.UTF8.GetBytes(message);
+        foreach (TcpClient client in tcpClients) {
+            client.GetStream().Write(encodedString, 0, encodedString.Length);
+        }
+    }
+
+    public void BeginRecievingMessages(TcpClient tcpClient) {
+        while(true) {
+            byte[] inputBuffer = new byte[tcpClient.ReceiveBufferSize];
+            int bytesRead = tcpClient.GetStream().Read(inputBuffer, 0, inputBuffer.Length);
+
+            if (bytesRead > 0) {
+                string recievedMsg = Encoding.UTF8.GetString(inputBuffer, 0, bytesRead);
+                SendMessage(recievedMsg, tcpClients.Except(new List<TcpClient>(){tcpClient}).ToArray());
+            }
+        }
+    }
+}
